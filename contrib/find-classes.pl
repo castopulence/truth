342e90996a7da89adfc7c67fe2155ae15587ca0d
Brandon McCaig <bamccaig@gmail.com>
2021-10-19 06:24:34 -0400

    Failed attempt to reimplement using package.
    I'm not familiar with Text::Balanced, and it proves to be a bit more
    low-level than I had hoped. So I'll either need to learn how to use it
    or find a higher level package to suit my needs. That or someone else
    will have to finish this branch for me. Whatever comes first.

--- contrib/find-classes.pl
+++ contrib/find-classes.pl
@@ -1,11 +1,13 @@
 use v5.032;
 
+use Text::Balanced qw/extract_delimited/;
+
 our %classes;
 
 while (my $line = <STDIN>) {
   chomp($line);
 
-  my @values = $line =~ m{ class \s* = \s* " ([^"]*) " }gx;
+  my @values = extract_delimited($line, "\"'", qr/.*\bclass=/);
 
   for my $value (map split(/ /), @values) {
     $classes{$value} = 1;
