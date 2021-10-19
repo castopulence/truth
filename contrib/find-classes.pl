a78988b04dece345395f4431dc9dd36cdcf5f34d
Brandon McCaig <bamccaig@gmail.com>
2021-10-19 06:40:39 -0400

    The latest (balanced-classes) isn't bad I guess.
    But it still leaves a few things to be desired that the original regexp
    doesn't.

--- contrib/find-classes.pl
+++ contrib/find-classes.pl
@@ -1,13 +1,16 @@
 use v5.032;
 
-use Text::Balanced qw/extract_delimited/;
+use Text::Balanced qw(extract_quotelike extract_multiple);
 
 our %classes;
 
 while (my $line = <STDIN>) {
   chomp($line);
 
-  my @values = extract_delimited($line, "\"'", qr/.*\bclass=/);
+  my @values = extract_multiple($line, [
+    \&extract_quotelike,
+    qr/class=/,
+    ]);
 
   for my $value (map split(/ /), @values) {
     $classes{$value} = 1;
