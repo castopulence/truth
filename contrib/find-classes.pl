957f2fbe4b1359b797e1c8fc0001689a7865a562
Brandon McCaig <bamccaig@gmail.com>
2021-10-19 06:52:47 -0400

    Further experiments that do not appear to work.

--- contrib/find-classes.pl
+++ contrib/find-classes.pl
@@ -1,16 +1,14 @@
 use v5.032;
 
-use Text::Balanced qw(extract_quotelike extract_multiple);
+use Text::Balanced qw(extract_delimited extract_multiple);
 
 our %classes;
 
 while (my $line = <STDIN>) {
   chomp($line);
 
-  my @values = extract_multiple($line, [
-    \&extract_quotelike,
-    qr/class=/,
-    ]);
+  my @extractors = ( \&extract_classes );
+  my @values = extract_multiple($line, \@extractors);
 
   for my $value (map split(/ /), @values) {
     $classes{$value} = 1;
@@ -18,3 +16,7 @@ while (my $line = <STDIN>) {
 }
 
 say for sort { $a cmp $b } keys %classes;
+
+sub extract_classes {
+  extract_delimited($_[0], q('"), 'class=');
+}
